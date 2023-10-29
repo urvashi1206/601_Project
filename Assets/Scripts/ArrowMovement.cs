de7005dde4c454e3c491e4ddbc3af800ba69385c
Noya <qimiaocai011110@gmail.com>
2023-10-28 23:24:41 -0400

    added scene animation

--- Assets/Scripts/ArrowMovement.cs
+++ Assets/Scripts/ArrowMovement.cs
@@ -11,6 +11,7 @@ public class ArrowMovement : MonoBehaviour
     public float angle;
 
     public GameObject scene;
+    public float r_timer;
     //private float rotateSpeed = 180f;
 
     private Ray _ray;
@@ -18,88 +19,131 @@ public class ArrowMovement : MonoBehaviour
     private bool coroutineIsFinished = true;
     public GameObject character;
     public GameObject character_head;
+    public Rigidbody c_rigidbody;
+    bool onrotation;
+    float rx, ry, rz;
+    public CharacterMovement character_movement;
+    Vector3 dir;
+    Quaternion r_des;
     // Start is called before the first frame update
     void Start()
     {
         m_Camera = Camera.main;
         character = GameObject.Find("character");
         character_head = GameObject.Find("head");
+        c_rigidbody = character.GetComponent<Rigidbody>();
+        r_timer = 0;
+        onrotation = false;
+        rx = 0; ry = 0; rz = 0;
+        r_des = scene.transform.rotation;
+        character_movement = GameObject.FindObjectOfType<CharacterMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.GetMouseButtonDown(0))
+        if (!onrotation)
         {
-            _ray =m_Camera.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(_ray, out _hit, 1000f))
+            if (Input.GetMouseButtonDown(0))
             {
-                if (_hit.transform == transform)
+                _ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(_ray, out _hit, 1000f))
                 {
-                    if (gameObject.CompareTag("X")) 
+                    if (_hit.transform == transform)
                     {
-                        if (coroutineIsFinished)
+                        if (gameObject.CompareTag("X"))
                         {
-                            StartCoroutine("DisableXButtonsBoxCollider");
-                            StartCoroutine("DisableYButtonsBoxCollider");
-                            StartCoroutine("DisableZButtonsBoxCollider");
+                            if (coroutineIsFinished)
+                            {
+                                StartCoroutine("DisableXButtonsBoxCollider");
+                                StartCoroutine("DisableYButtonsBoxCollider");
+                                StartCoroutine("DisableZButtonsBoxCollider");
+                            }
+                            else
+                            {
+                                //If the coroutine is still going on we stop it
+                                StopCoroutine("DisableXButtonsBoxCollider");
+                                StopCoroutine("DisableYButtonsBoxCollider");
+                                StopCoroutine("DisableZButtonsBoxCollider");
+                            }
+                            //character facing fall side
+                            dir = new Vector3(0, 0, angle);
+                            character.transform.LookAt(character.transform.position + dir);
+                            character_head.transform.LookAt(character_head.transform.position + dir);
+                            c_rigidbody.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+                            //level rotation
+                            r_des = Quaternion.Euler(angle, 0, 0) * scene.transform.rotation;
+                            onrotation = true;
+                            character_movement.onrotation = true;
+                            rx = angle; ry = 0; rz = 0;
                         }
-                        else
+                        else if (gameObject.CompareTag("Y"))
                         {
-                            //If the coroutine is still going on we stop it
-                            StopCoroutine("DisableXButtonsBoxCollider");
-                            StopCoroutine("DisableYButtonsBoxCollider");
-                            StopCoroutine("DisableZButtonsBoxCollider");
+                            if (coroutineIsFinished)
+                            {
+                                StartCoroutine("DisableXButtonsBoxCollider");
+                                StartCoroutine("DisableYButtonsBoxCollider");
+                                StartCoroutine("DisableZButtonsBoxCollider");
+                            }
+                            else
+                            {
+                                //If the coroutine is still going on we stop it
+                                StopCoroutine("DisableXButtonsBoxCollider");
+                                StopCoroutine("DisableYButtonsBoxCollider");
+                                StopCoroutine("DisableZButtonsBoxCollider");
+                            }
+                            c_rigidbody.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+                            r_des = Quaternion.Euler(0, angle, 0) * scene.transform.rotation;
+                            onrotation = false;
+                            character_movement.onrotation = true;
+                            rx = 0; ry = angle; rz = 0;
                         }
-                        //character facing fall side
-                        Vector3 dir = new Vector3(0, 0, angle);
-                        character.transform.LookAt(character.transform.position + dir);
-                        character_head.transform.LookAt(character_head.transform.position + dir);
-                        //level rotation
-                        scene.transform.rotation = Quaternion.Euler(angle, 0, 0) * scene.transform.rotation;
-                    }
-                    else if (gameObject.CompareTag("Y"))
-                    {
-                        if (coroutineIsFinished)
+                        else if (gameObject.CompareTag("Z"))
                         {
-                            StartCoroutine("DisableXButtonsBoxCollider");
-                            StartCoroutine("DisableYButtonsBoxCollider");
-                            StartCoroutine("DisableZButtonsBoxCollider");
+                            if (coroutineIsFinished)
+                            {
+                                StartCoroutine("DisableXButtonsBoxCollider");
+                                StartCoroutine("DisableYButtonsBoxCollider");
+                                StartCoroutine("DisableZButtonsBoxCollider");
+                            }
+                            else
+                            {
+                                //If the coroutine is still going on we stop it
+                                StopCoroutine("DisableXButtonsBoxCollider");
+                                StopCoroutine("DisableYButtonsBoxCollider");
+                                StopCoroutine("DisableZButtonsBoxCollider");
+                            }
+                            //character facing fall side
+                            dir = new Vector3(-angle, 0, 0);
+                            character.transform.LookAt(character.transform.position + dir);
+                            character_head.transform.LookAt(character_head.transform.position + dir);
+                            c_rigidbody.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+                            //level rotation
+                            r_des = Quaternion.Euler(0, 0, angle) * scene.transform.rotation;
+                            onrotation = true;
+                            character_movement.onrotation = true;
+                            rx = 0; ry = 0; rz = angle;
                         }
-                        else
-                        {
-                            //If the coroutine is still going on we stop it
-                            StopCoroutine("DisableXButtonsBoxCollider");
-                            StopCoroutine("DisableYButtonsBoxCollider");
-                            StopCoroutine("DisableZButtonsBoxCollider");
-                        }
-                        scene.transform.rotation = Quaternion.Euler(0, angle, 0) * scene.transform.rotation;
-                    }
-                    else if(gameObject.CompareTag("Z"))
-                    {
-                        if (coroutineIsFinished)
-                        {
-                            StartCoroutine("DisableXButtonsBoxCollider");
-                            StartCoroutine("DisableYButtonsBoxCollider");
-                            StartCoroutine("DisableZButtonsBoxCollider");
-                        }
-                        else
-                        {
-                            //If the coroutine is still going on we stop it
-                            StopCoroutine("DisableXButtonsBoxCollider");
-                            StopCoroutine("DisableYButtonsBoxCollider");
-                            StopCoroutine("DisableZButtonsBoxCollider");
-                        }
-                        //character facing fall side
-                        Vector3 dir = new Vector3(-angle, 0, 0);
-                        character.transform.LookAt(character.transform.position + dir);
-                        character_head.transform.LookAt(character_head.transform.position + dir);
-                        //level rotation
-                        scene.transform.rotation = Quaternion.Euler(0, 0, angle) * scene.transform.rotation;
                     }
                 }
+            }
+        }
+        else
+        {
+            if(r_timer > 1)
+            {
+                r_timer = 0;
+                onrotation = false;
+                character_movement.onrotation = false;
+                scene.transform.rotation = r_des;
+                c_rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+                c_rigidbody.velocity = Vector3.zero;
+            }
+            else
+            {
+                r_timer += Time.deltaTime;
+                scene.transform.rotation = Quaternion.Euler(rx * Time.deltaTime, ry * Time.deltaTime, rz * Time.deltaTime) * scene.transform.rotation;
             }
         }
     }
