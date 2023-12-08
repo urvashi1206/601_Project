266b3085ffc5c80d7bf33b744892309665160805
urvashi1206 <dhingraurvashi99@gmail.com>
2023-12-07 23:16:14 -0500

    Reset fixed

--- Assets/Scripts/ResetButton.cs
+++ Assets/Scripts/ResetButton.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ResetButton : MonoBehaviour
 {
-    [SerializeField] private Button resetButton;
+    /*[SerializeField] private Button resetButton;
     public GameObject character;
     public GameObject scene;
     public Quaternion character_rotation;
     public Vector3 character_position;
     public Quaternion scene_rotation;
-    public Vector3 scene_position;
+    public Vector3 scene_position;*/
     // Start is called before the first frame update
     void Start()
     {
-        resetButton = GetComponent<Button>();
+        /*resetButton = GetComponent<Button>();
         resetButton.onClick.AddListener(ResetRoomRotation);
         resetButton.onClick.AddListener(ResetCharacterPosition);
         
@@ -25,10 +26,10 @@ public class ResetButton : MonoBehaviour
         character_position = character.transform.position;
         scene = GameObject.Find("Level_Anchor");
         scene_rotation = scene.transform.rotation;
-        scene_position = scene.transform.position;
+        scene_position = scene.transform.position;*/
     }
 
-    private void ResetCharacterPosition()
+    /*private void ResetCharacterPosition()
     {
         Debug.Log("ResetCharacterPosition");
         character.transform.position = character_position;
@@ -41,6 +42,12 @@ public class ResetButton : MonoBehaviour
         Debug.Log("ResetRoomRotation");
         scene.transform.rotation = scene_rotation;
         scene.transform.position = scene_position;
+    }*/
+
+    public void ResetLevel(string scenename)
+    {
+        SceneManager.UnloadSceneAsync(gameObject.scene);
+        SceneManager.LoadScene(scenename, LoadSceneMode.Additive);
     }
     
 }
