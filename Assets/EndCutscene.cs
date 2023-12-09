7ef0db9df9b83a2208a2b3ba694add88f4d76107
AnnieZh26 <2663254976@qq.com>
2023-12-09 16:00:55 -0500

    Ending Scene Script

--- Assets/EndCutscene.cs
+++ Assets/EndCutscene.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndCutscene : MonoBehaviour
 {
     public Animator anim;
+    public Animator CurtainAnim;
+
+    [SerializeField] private Button ClickEvent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ClickEvent = GameObject.Find("Newspaper").GetComponent<Button>();
+        ClickEvent.onClick.AddListener(IsClicked);
     }
 
     // Update is called once per frame
@@ -17,9 +22,20 @@ public class EndCutscene : MonoBehaviour
     {
         AnimatorStateInfo animationInfo;
         animationInfo = anim.GetCurrentAnimatorStateInfo(0);
-        if ((animationInfo.normalizedTime > 1.0f) && (animationInfo.IsName("Blackout")))//animation end
+        if (animationInfo.normalizedTime > 1.0f)//check if animation has end
         {
-            SceneManager.LoadScene("MainMenu");
+            Debug.Log("Anim end.");
+
         }
+    }
+
+    private void IsClicked()
+    {
+        // Click the newspaper and then set the fade_out parameter to true
+        Debug.Log("Click happens");
+
+        CurtainAnim.SetTrigger("ClickNewspaper");
+
+        SceneManager.LoadScene("MainMenu");
     }
 }
